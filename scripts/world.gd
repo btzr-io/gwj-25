cdc4af19866fde75babcb02c791be93e01f22447
btzr-io <btzr.io@gmail.com>
2020-09-18 17:36:15 -0500

    add dark / light world example

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,8 +5,8 @@ var playerInitialPosition: Vector2
 
 func _ready():
 	GM.init_level()
-	$Key.connect("body_entered", self, "_on_key_body_entered")
-	$Portal.connect("body_entered", self, "_on_portal_body_entered")
+	$Light_world/Key.connect("body_entered", self, "_on_key_body_entered")
+	$Light_world/Portal.connect("body_entered", self, "_on_portal_body_entered")
 	$Player.connect("player_die", self, "_on_player_die")
 	playerInitialPosition = $Player.position
 	
@@ -14,8 +14,8 @@ func _ready():
 func _on_key_body_entered(body):
 	if body == $Player:
 		playerHasKey = true
-		$Key.visible = false
-		$Key.disconnect("body_entered", self, "_on_key_body_entered")
+		$Light_world/Key.visible = false
+		$Light_world/Key.disconnect("body_entered", self, "_on_key_body_entered")
 	pass
 	
 func _on_portal_body_entered(body):
